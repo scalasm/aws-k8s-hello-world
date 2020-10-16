a92efd0e138f1da7f7828328571ddf21d48c9ad3
Mario Scalas <mario.scalas@gmail.com>
2020-10-16 10:50:29 +0200

    Testing and less verbose logging

---
+++ src/test/java/me/marioscalas/helloworld/HelloWorldControllerTest.java
@@ -0,0 +1,37 @@
+package me.marioscalas.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.hamcrest.MockitoHamcrest.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest( HelloWorldController.class )
+class HelloWorldControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private HelloWorldController controller;
+
+    @Test
+    void getSalute() throws Exception {
+        // given no special pre-conditions
+
+        // when
+        mvc.perform( get("/hello")
+                    .accept( MediaType.APPLICATION_JSON)
+                )
+        // then
+                .andExpect(status().isOk())
+                .andExpect( jsonPath( "$.salute", is( "Hello Kubernetes World!" ) ));
+    }
+}
