246b2c777b829e681394e28d97757da39ef7e7e9
Mario Scalas <mario.scalas@gmail.com>
2023-08-07 20:50:04 +0100

    Explicitly set applicatoinb/json for the /hello endpoint

--- src/main/java/me/marioscalas/helloworld/HelloWorldController.java
+++ src/main/java/me/marioscalas/helloworld/HelloWorldController.java
@@ -1,5 +1,6 @@
 package me.marioscalas.helloworld;
 
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ record SaluteResponse(String salute) {}
 @RequestMapping( "/hello" )
 public class HelloWorldController {
 
-    @GetMapping
+    @GetMapping(produces = MimeTypeUtils.APPLICATION_JSON_VALUE)
     public SaluteResponse getSalute() {
         return new SaluteResponse( "Hello Kubernetes World from pod: " + System.getenv("MY_POD_NAME") );
     }
